9a8f468365661ccd429fc467e75e60826fce9a59
Mohd Shahid <sid@github.com>
2020-11-08 20:38:48 +0530

    click to copy

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ if (message) {
 
 document.querySelector('form').addEventListener('submit', (e) => {
 	e.preventDefault();
+	const linkInput = document.querySelector('#link-input');
+	const input = document.querySelector('#message-input');
+	const encrypted = btoa(input.value);
 
 	document.querySelector('#message-form').classList.add('hide');
 	document.querySelector('#link-form').classList.remove('hide');
 
-	const input = document.querySelector('#message-input');
-
-	const encrypted = btoa(input.value);
-
-	const linkInput = document.querySelector('#link-input');
-
 	linkInput.value = `${window.location}#${encrypted}`;
 	linkInput.select();
+	linkInput.addEventListener('click', () => {
+		document.execCommand('copy');
+	});
 });
